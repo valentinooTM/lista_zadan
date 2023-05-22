4070198fffe1eb8a17e5327b59d42bf1f31a136f
Uczen <Uczen@K5>
2023-05-22 09:11:47 +0200

    Dodaj pliki projektów.

---
+++ lista_zadan/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lista_zadan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void taskName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void addTask(object sender, RoutedEventArgs e)
+        {
+            String nazwaZadania = taskName.Text.ToString(); 
+            String kategoriaZadania = taskCategory.Text.ToString();
+            DateTime? terminZadania = taskDeadline.SelectedDate;
+
+            MessageBox.Show(nazwaZadania, kategoriaZadania);
+            
+            
+        }
+    }
+}
